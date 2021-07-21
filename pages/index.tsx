b46d428c67800fde180468e2f912da8accfd5214
rohmatsyam <rohmatsyam@yahoo.com>
2021-07-22 03:28:46 +0700

    nextjs fundamental

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,8 @@
-const index = () => {
+import { GetServerSidePropsContext, GetStaticPropsContext } from 'next'
+import {services} from '../data'
+
+const index = ({services}) => {
+  console.log("Client",services)
   return (
     <div>
       <h1>Hello World</h1>
@@ -7,3 +11,29 @@ const index = () => {
 }
 
 export default index
+
+// export const getServerSideProps = async (contect:GetServerSidePropsContext) =>{
+//   //calculation
+//   const res = await fetch('http://localhost:3000/api/services')
+//   const data = await res.json()
+// console.log("server", services)
+
+//   return {
+//     props:{
+//       services:data.services
+//     }
+//   }
+// }
+
+export const getServerStaticProps = async (contect:GetStaticPropsContext) =>{
+  //calculation
+  const res = await fetch('http://localhost:3000/api/services')
+  const data = await res.json()
+console.log("server", services)
+
+  return {
+    props:{
+      services:data.services
+    }
+  }
+}
