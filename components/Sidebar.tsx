b8faaa3f766370f4dd6ab95b50ecc854cac79518
rohmatsyam <rohmatsyam@yahoo.com>
2021-07-22 23:42:54 +0700

    add dark mode

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,18 +1,21 @@
 import {AiFillGithub,AiFillInstagram,AiFillFacebook} from 'react-icons/ai'
 import {GoLocation} from 'react-icons/go'
 import {GiTie} from 'react-icons/gi'
-
+import Image from 'next/image'
+import {useTheme} from 'next-themes'
 
 const Sidebar = () => {
+    const {theme,setTheme} = useTheme();        
+
     return (
-        <div>
-            <img src="/img/profil.jpg" alt="user avatar" className="w-32 h-32 mx-auto rounded-full"/>
+        <>
+            <Image src="/img/profil.jpg" alt="user avatar" width={128} height={128} className="mx-auto rounded-full"/>
             <h3 className="mt-4 text-3xl font-medium tracking-wider font-kaushan min-w-max xl:text-base">
                 <span className="text-green">Rohmat </span>
                 Syamsul Huda
             </h3>
-            <p className="px-2 py-1 my-3 bg-gray-200 rounded-full">Web Developer</p>
-            <a className="flex items-center justify-center px-2 py-1 my-3 bg-gray-200 rounded-full"
+            <p className="px-2 py-1 my-3 bg-gray-200 rounded-full dark:bg-dark-200">Web Developer</p>
+            <a className="flex items-center justify-center px-2 py-1 my-3 bg-gray-200 rounded-full dark:bg-dark-200"
             href="" download="name">
                 <GiTie className="w-6 h-6"/>Download Resume
             </a>
@@ -31,7 +34,7 @@ const Sidebar = () => {
             </div>
 
             {/* Address */}
-            <div className="py-4 my-5 bg-gray-200 min-w-max" style={{marginLeft:'-1rem',marginRight:'-1rem'}}>
+            <div className="py-4 my-5 bg-gray-200 dark:bg-dark-200 min-w-max" style={{marginLeft:'-1rem',marginRight:'-1rem'}}>
                 <div className="flex items-center justify-center space-x-2">
                     <GoLocation/>
                     <span>Trenggalek, Indonesia</span>
@@ -43,10 +46,11 @@ const Sidebar = () => {
             {/* Email Button */}
             <div>
             <button className="w-8/12 px-4 py-2 my-1 text-white rounded-full xl:w-10/12 bg-gradient-to-r from-green to-blue-400 focus:outline-none" onClick={()=>window.open('mailto:rohmatsyam@yahoo.com')}>Email Me</button>
-            <button className="w-8/12 px-4 py-2 my-1 text-white rounded-full xl:w-10/12 bg-gradient-to-r from-green to-blue-400">Toggle Theme</button>
+            <button className="w-8/12 px-4 py-2 my-1 text-white rounded-full xl:w-10/12 bg-gradient-to-r from-green to-blue-400 focus:outline-none" onClick={() => {
+            setTheme(theme === 'light' ? 'dark' : 'light')
+          }}>Toggle Theme</button>
             </div>
-
-        </div>
+        </>
     )
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,21 @@ import Sidebar from '../components/Sidebar'
 import Navbar from '../components/Navbar'
 import '../styles/globals.css'
 
+import {ThemeProvider} from 'next-themes'
+
 function MyApp({ Component, pageProps }) {
   return (
-  <div className="grid grid-cols-12 gap-6 px-4 mx-auto my-4 lg:px-48 sm:px-20 md:px-30">
-    <div className="col-span-12 p-4 text-center bg-white xl:col-span-3 rounded-2xl">
-      <Sidebar/>
-    </div>
-    <div className="flex flex-col col-span-12 overflow-hidden bg-white xl:col-span-9 rounded-2xl">
-    <Navbar/>
-    <Component {...pageProps} />
-    </div>
-  </div>
+    <ThemeProvider forcedTheme={Component.theme || undefined} attribute="class">
+      <div className="grid grid-cols-12 gap-6 px-4 mx-auto my-4 lg:px-48 sm:px-20 md:px-30">
+        <div className="col-span-12 p-4 text-center bg-white dark:bg-dark-500 xl:col-span-3 rounded-2xl shadow-custom-light dark:shadow-custom-dark">
+          <Sidebar/>
+        </div>
+        <div className="flex flex-col col-span-12 overflow-hidden bg-white xl:col-span-9 rounded-2xl dark:bg-dark-500 shadow-custom-light dark:shadow-custom-dark">
+        <Navbar/>
+        <Component {...pageProps} />
+        </div>
+      </div>
+    </ThemeProvider>
   )
 }
 
